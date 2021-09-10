0a85b9dd413ebe8a327a949562597fb85ac82449
zarminta <18102217@ittelkom-pwt.ac.id>
2021-09-10 15:33:02 +0700

    form component

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -3,6 +3,26 @@
  *
  * */
 
-const MealItemForm = () => {};
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
+
+const MealItemForm = () => {
+  return (
+    <form className={classes.form}>
+      <Input
+        lavel="Amount"
+        input={{
+          id: "amount",
+          type: "text",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
 
 export default MealItemForm;
